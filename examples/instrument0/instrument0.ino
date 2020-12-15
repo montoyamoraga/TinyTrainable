215236ce7b939f815d49e914073dedbf761c3037
montoyamoraga <montoyamoraga@gmail.com>
2020-12-15 14:18:20 -0500

    add example

--- examples/instrument0/instrument0.ino
+++ examples/instrument0/instrument0.ino
@@ -1,9 +1,17 @@
+#include "Arduino.h"
+#include "TinyTrainable.h"
+
+TinyTrainable myInst(true);
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  myInst.debugPrint("Arduino k-NN color classifier");
+  myInst.setLabels("Object 0", "Object 1", "Object 2");
+  myInst.trainKNN(5, 10, 0.5);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  myInst.identify();
 }

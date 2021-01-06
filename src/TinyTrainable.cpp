7e49c08edc4f744ac992ead4924d8733214ef86e
montoyamoraga <montoyamoraga@gmail.com>
2021-01-06 14:12:34 -0500

    v0.0.3 with new structures by peter

--- src/TinyTrainable.cpp
+++ src/TinyTrainable.cpp
@@ -1,23 +1,29 @@
-#include <Arduino.h>
-#include <Arduino_KNN.h>
+// include local library
 #include "TinyTrainable.h"
 
-// constructor function
-// peter proposed the syntax for this constructor
-// where the myKNN is initialized here
-// TODO: add docs for what initialization lists are
-TinyTrainable::TinyTrainable(int number, int inputs, int K): _myKNN(3)  {
-  _number = number;
-  _inputs = inputs;
-  _K = K;
-  //  _myKNN.KNNClassifier(3);
+// constructor for the TinyTrainable class
+TinyTrainable::TinyTrainable()
+{
+  _serialDebugging = true;  // hardcoded for now
 }
 
-void TinyTrainable::setupSerial() {
-  Serial.begin(9600);
-  while (!Serial);
+// sets up the LEDS on the Arduino Nano 33 BLE Sense
+void TinyTrainable::setupLED()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+
+  // start with everything off
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LEDR, HIGH);
+  digitalWrite(LEDG, HIGH);
+  digitalWrite(LEDB, HIGH);
 }
 
+// TODO - note which pins
+// sets up Serial1 (serial output over pins)
 void TinyTrainable::setupSerial1() {
   Serial1.begin(9600);
 
@@ -31,88 +37,27 @@ void TinyTrainable::setupSerial1() {
   *pointerBaudrate = baudrate;
 }
 
-void TinyTrainable::setupBuiltInLED() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LEDR, OUTPUT);
-  pinMode(LEDG, OUTPUT);
-  pinMode(LEDB, OUTPUT);
-
-  // start with everything off
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(LEDG, HIGH);
-  digitalWrite(LEDB, HIGH);
-
-}
-
-void TinyTrainable::setColorBuiltInLED(int color) {
+// sets the color of the built in LED on the Arduino Nano 33 BLE Sense
+void TinyTrainable::setColorBuiltInLED(Colors color) {
   // turn everything off
   digitalWrite(LEDR, HIGH);
   digitalWrite(LEDG, HIGH);
   digitalWrite(LEDB, HIGH);
 
-  // 0 red
-  if (color == 0) {
+  if (color == red) {
     digitalWrite(LEDR, LOW);
-  }
-  // 1 green
-  else if (color == 1) {
+  } else if (color == green) {
     digitalWrite(LEDG, LOW);
-  }
-  // 2 blue
-  else if (color == 2) {
-    digitalWrite(LEDB, LOW);
-  }
-  // 3 yellow = red + green
-  else if (color == 3) {
-    digitalWrite(LEDR, LOW);
-    digitalWrite(LEDG, LOW);
-  }
-  // 4 magenta = red + blue
-  else if (color == 4) {
-    digitalWrite(LEDR, LOW);
-    digitalWrite(LEDB, LOW);
-  }
-  // 5 cyan = green + blue
-  else if (color == 5 ) {
-    digitalWrite(LEDG, LOW);
+  } else if (color == blue) {
     digitalWrite(LEDB, LOW);
   }
 }
 
-void TinyTrainable::readInertia() {
-  float inertiaX, inertiaY, inertiaZ, proximity, inertiaTotal = 0.0;
-
-  // Wait for the object to move close
-  while (!APDS.proximityAvailable() || APDS.readProximity() > 0) {}
-
-  // Sample if color is available and object is close
-  while (!IMU.accelerationAvailable());
-
-  // Read color and proximity
-  IMU.readAcceleration(inertiaX, inertiaY, inertiaZ);
-  inertiaTotal = (inertiaX + inertiaY + inertiaZ);
-
-  _inertia[0] = inertiaX;
-  _inertia[1] = inertiaY;
-  _inertia[2] = inertiaZ;
-
-  if (_outputDebug) {
-    Serial.println(inertiaTotal);
-    Serial.print(_inertia[0]);
-    Serial.print(",");
-    Serial.print(_inertia[1]);
-    Serial.print(",");
-    Serial.println(_inertia[2]);
-  }
-}
-
-void TinyTrainable::addNewExample(int currentClass) {
-  _myKNN.addExample(_inertia, currentClass);
-}
-
-int TinyTrainable::classifyInput() {
-  // TODO: why does this line not work :(
-  //  return _myKNN.classify(_inertia, _K);
-  return 0;
+// send 3 byte midi message over Serial1 (pins)
+// 'midiNote' is the midi note number in decimal
+// midi channel and note velocity are preset
+void TinyTrainable::midiCommand(byte midiNote) {
+  Serial1.write(_midiChannelDec);
+  Serial1.write(midiNote);
+  Serial1.write(_midiVelocity);
 }
--- src/TinyTrainable.h
+++ src/TinyTrainable.h
@@ -1,69 +1,41 @@
 // TinyTrainable.h - a library for Tiny Trainable Instruments
-// code by aaron montoya-moraga
-// with assistance by peter tone
-// for Arduino Nano 33 BLE Sense
-// programmed using Arduino IDE 1.8.13
-// on a Macbook Air with MacOS Catalina 10.15.7
-// started in november 2020
+// a project by Aarón montoya-moraga
+// with assistance by Peter Tone
+// Started in November 2020
 // MIT License
 
-// tip:
-// if you are having trouble uploading the sketch,
-// press fast twice the reset button on the arduino
+#ifndef _TINY_LIBRARY_H_
+#define _TINY_LIBRARY_H_
 
-#ifndef TinyTrainable_h
-#define TinyTrainable_h
+// include Arduino library
+#include <Arduino.h>
 
-// include library dependencies
-#include "Arduino.h"
-
-// include Arduino KNN library
-#include <Arduino_KNN.h>
-
-// include Arduino library for IMU sensor
-// more information at
-// https://www.arduino.cc/en/Reference/ArduinoLSM9DS1
-#include <Arduino_LSM9DS1.h>
-
-// include Arduino library for proximity sensor
-// more information at
-// https://www.arduino.cc/en/Reference/ArduinoAPDS9960
-#include <Arduino_APDS9960.h>
+// colors for setting the embedded LED
+enum Colors {red, green, blue};
 
 class TinyTrainable {
   public:
-    TinyTrainable(int number, int inputs, int K);
-    void setupSerial();
-    void setupSerial1();
-    void setupBuiltInLED();
-    void setColorBuiltInLED(int color);
-    void readInertia();
-    void addNewExample(int currentClass);
-    int classifyInput();
+    TinyTrainable();
+    
+    // template datatypes allows any datatype as an argument, like Serial.println().
+    // it is defined here in the header file so it compiles at the beginning
+    // and it can be used on inst0.cpp, among other
+    template <typename T> void debugPrint(T message) {
+      if (_serialDebugging) {
+        Serial.println(message);
+      }
+    };
 
-  private:
-    // integer to choose what type of instrument is it
-    int _number;
-    // integer to choose how many inputs to the KNN algorithm
-    int _inputs;
-    // float array to store data to pass to the KNN library
-    // TODO: make the length of _inertia be dependent on inputs
-    float _inertia[3];
-    // KNN classifier TODO: make the length of _inertia be dependent on inputs
-    KNNClassifier _myKNN;
-    // K number for KNN classifier
-    int _K;
-    // integer for current class
-    int _currentClass;
-    // this boolean is for turning on/off
-    // the output of KNN classes via hardware (pins of the Arduino)
-    boolean _outputHardware = true;
-    // this boolean is for turning on/off
-    // the output of KNN classes via USB Serial port
-    boolean _outputUSB = false;
-    // this boolean is for turning on/off
-    // the output of debugging messages USB Serial port
-    boolean _outputDebug = false;
+  protected:
+    static void setupLED();
+    void setupSerial1();
+    void setColorBuiltInLED(Colors color);
+    void midiCommand(byte midiNote);
+
+    bool _serialDebugging;
+    byte _midiChannelDec;
+    byte _midiVelocity;
+    //    int _outputPin;
 };
 
 #endif

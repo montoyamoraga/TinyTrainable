c1a6d7d0fe5b1d1a38d2b8125faa065dc836c05f
montoyamoraga <montoyamoraga@gmail.com>
2021-01-07 14:48:32 -0500

    add draft example midi

---
+++ examples/inst0_midi/inst0_midi.ino
@@ -0,0 +1,36 @@
+// include libraries
+#include "TinyTrainable.h"
+#include "Inst0.h"
+
+int snare = 38;
+
+void setup() {
+
+  Serial1.begin(9600);
+
+  // desired baudrate
+  uint32_t baudrate = 0x800000;
+
+  // pointer to the memory address that stores the baudrate
+  uint32_t *pointerBaudrate = ( uint32_t * )0x40002524;
+
+  // replace the value at the pointer with the desired baudrate
+  *pointerBaudrate = baudrate;
+}
+
+void loop() {
+
+  // 153
+  // note on channel 10
+  // 143 + channel
+  midiCommand(0x99, snare, 127);
+
+  delay(5000);
+}
+
+// send 3 byte midi message
+void midiCommand(byte cmd, byte data1, byte data2) {
+  Serial1.write(cmd);
+  Serial1.write(data1);
+  Serial1.write(data2);
+}

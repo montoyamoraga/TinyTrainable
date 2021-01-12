aa4432da80cf70b4a1538aa1a94861e21346e1e4
montoyamoraga <montoyamoraga@gmail.com>
2021-01-11 23:32:18 -0500

    add placeholder Inst2 files

---
+++ src/Inst2.h
@@ -0,0 +1,15 @@
+#ifndef INST2_H
+#define INST2_H
+
+#include <Arduino.h>
+#include "TinyTrainable.h"
+
+// inheritance
+class Inst2:public TinyTrainable {
+  public:
+    Inst2();
+    void print2();
+  
+};
+
+#endif

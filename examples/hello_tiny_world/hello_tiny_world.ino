0b52feeee32a76e036ec7446e99b7b86745bef4f
montoyamoraga <montoyamoraga@gmail.com>
2021-01-14 16:51:10 -0500

    change name of example

--- examples/hello_tiny_world/hello_tiny_world.ino
+++ examples/hello_tiny_world/hello_tiny_world.ino
@@ -3,15 +3,18 @@
 
 // this sketch cycles through all the possible LED colors
 
-// include library
+// include all libraries
 #include "Inst0.h"
+#include "Inst1.h"
+#include "Inst2.h"
 
 // variables for time LEDs are on and off
 // measured in milliseconds
 int timeOn = 500;
 int timeOff = 250;
 
-// declare instance of the instrument0
+// declare instance of the instrument
+// default is Inst0, change Inst0 to Inst1 or Inst2 if necessary
 Inst0 tiny;
 
 void setup() {

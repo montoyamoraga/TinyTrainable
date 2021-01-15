4cd88621a1f02602cca7de4b8e44bfc648a540d0
montoyamoraga <montoyamoraga@gmail.com>
2021-01-14 22:46:01 -0500

    copy from inst0

--- examples/inst1_buzzer/inst1_buzzer.ino
+++ examples/inst1_buzzer/inst1_buzzer.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+// include library
+#include <Inst1.h>
 
+// declare instance of the instrument0
+Inst1 tiny;
+
+// constants for the KNN algorithm
+const int K = 5;
+const int EXAMPLES_PER_CLASS = 10;
+const float COLOR_THRESHOLD = 0.5;
+
+// setup() runs once, at the beginning
+void setup() {
+
+  // configure all LEDs on the microcontroller
+  tiny.setupLEDs();
+
+  // turn on the sensor to measure
+  // gestures, color, light intensity and proximity
+  tiny.setupSensorAPDS9960();
+
+  // setup instrument to output over midi
+  tiny.setupInstrument(pinOut);
+
+  // set pin for buzzer and note length
+  tiny.setupPin(8, 1000);
+
+  // set note frequencies for buzzer
+  tiny.setFrequencies(1000, 1200, 1400);
+
+  // set labels for each of the three classes
+  tiny.setLabels("Object 0", "Object 1", "Object 2");
+
+  // train the KNN algorithm
+  tiny.trainKNN(K, EXAMPLES_PER_CLASS, COLOR_THRESHOLD);
 }
 
+// loop() runs after setup(), on a loop
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // identify the input and respond
+  tiny.identify();
 }

c0e7d0bf8902249ac588f588172468f7bd8887e5
montoyamoraga <montoyamoraga@gmail.com>
2021-01-26 15:00:38 -0500

    update example to newer functions for frequency and duration

--- examples/inst0_buzzer/inst0_buzzer.ino
+++ examples/inst0_buzzer/inst0_buzzer.ino
@@ -21,7 +21,29 @@ const bool tinyDebugging = true;
 void setup() {
   tiny.setupInstrument(tinyDebugging);
 
-  tiny.setupOutputBuzzer(outputPin, 1000, 1000, 1200, 1400);
+  tiny.setupOutputBuzzer(outputPin);
+  // TODO: delete old version
+  // tiny.setupOutputBuzzer(outputPin, 1000, 1000, 1200, 1400);
+
+  // set fixed frequencies
+  tiny.setupBuzzerFrequency(0, 1000);
+  tiny.setupBuzzerFrequency(1, 1200);
+  tiny.setupBuzzerFrequency(2, 1400);
+
+  // or define ranges of frequencies
+  // tiny.setupBuzzerFrequency(0, 1000, 1500);
+  // tiny.setupBuzzerFrequency(1, 2000, 2500);
+  // tiny.setupBuzzerFrequency(2, 3000, 3500);
+
+  // or define arrays of frequencies
+  // tiny.setupBuzzerFrequency(0, [1000, 1500]);
+  // tiny.setupBuzzerFrequency(1, 2000, 2500);
+  // tiny.setupBuzzerFrequency(2, 3000, 3500);
+
+  // set duratons, easy
+  tiny.setupBuzzerDuration(0, 1000);
+  tiny.setupBuzzerDuration(1, 1000);
+  tiny.setupBuzzerDuration(2, 1000);
 
   String objectNames[3] = {"Object 0", "Object 1", "Object 2"};
   // train the KNN algorithm

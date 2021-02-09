57fa639d2fbb3a7f339557d45c1599c38a27bafb
montoyamoraga <montoyamoraga@users.noreply.github.com>
2021-02-09 20:59:01 +0000

    deploy: 70dcc2dbf089a039cf1d5c358554c76e26ae7ef1

--- search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['inst0_5fbuzzer_2eino_276',['inst0_buzzer.ino',['../index.html',1,'']]]
-];
--- search/searchdata.js
+++ search/searchdata.js
@@ -7,8 +7,7 @@ var indexSectionsWithContent =
   4: "_acefgkmnost",
   5: "bco",
   6: "bcgmorsuy",
-  7: "n",
-  8: "i"
+  7: "n"
 };
 
 var indexSectionNames =
@@ -20,8 +19,7 @@ var indexSectionNames =
   4: "variables",
   5: "enums",
   6: "enumvalues",
-  7: "defines",
-  8: "pages"
+  7: "defines"
 };
 
 var indexSectionLabels =
@@ -33,7 +31,6 @@ var indexSectionLabels =
   4: "Variables",
   5: "Enumerations",
   6: "Enumerator",
-  7: "Macros",
-  8: "Pages"
+  7: "Macros"
 };
 

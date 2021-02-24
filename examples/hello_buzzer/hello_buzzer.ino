f3ea690e87e3c09c190a94f156c1771e3c48780e
montoyamoraga <montoyamoraga@gmail.com>
2021-02-24 12:03:17 -0500

    add draft

--- examples/hello_buzzer/hello_buzzer.ino
+++ examples/hello_buzzer/hello_buzzer.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+/// @file hello_buzzer.ino
+/// @brief Test buzzer output
 
+// include all libraries
+#include <Inst0.h>
+#include <Inst1.h>
+#include <Inst2.h>
+
+// declare instance of the instrument
+// default is Inst0, change Inst0 to Inst1 or Inst2 if necessary
+Inst0 tiny;
+
+// constant for debugging
+// true: the instrument needs to be connected to serial port
+// false: the instrument is standalone
+const bool tinyDebugging = false;
+
+// setup() runs once, at the beginning
+void setup() {
+  // initialize instrument
+  tiny.setupInstrument(tinyDebugging);
 }
 
+// loop() runs in a loop, after setup()
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // TODO: add functions for testing buzzer
 }

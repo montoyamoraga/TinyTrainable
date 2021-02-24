299841ef4e90bd9281d96eb43eda749001cb8b81
montoyamoraga <montoyamoraga@gmail.com>
2021-02-24 12:14:56 -0500

    move some code to hello_buzzer

--- examples/hello_buzzer/hello_buzzer.ino
+++ examples/hello_buzzer/hello_buzzer.ino
@@ -15,10 +15,27 @@ Inst0 tiny;
 // false: the instrument is standalone
 const bool tinyDebugging = false;
 
+// constants for the hardware
+const int outputPin = 8;
+
+// arrays of frequencies
+int freqArray0[] = {1100, 1200, 1300, 1400, 1500, 1600};
+int freqArray1[] = {2100, 2200, 2300, 2400, 2500, 2600};
+int freqArray2[] = {3100, 3200, 3300, 3400, 3500, 3600};
+
 // setup() runs once, at the beginning
 void setup() {
   // initialize instrument
   tiny.setupInstrument(tinyDebugging);
+
+  tiny.setBuzzerFrequency(0, freqArray0, paramArrayLength);
+  tiny.setBuzzerFrequency(1, freqArray1, paramArrayLength);
+  tiny.setBuzzerFrequency(2, freqArray2, paramArrayLength);
+
+  // set durations, in milliseconds
+  tiny.setBuzzerDuration(0, 300);
+  tiny.setBuzzerDuration(1, 600);
+  tiny.setBuzzerDuration(2, 900);
 }
 
 // loop() runs in a loop, after setup()

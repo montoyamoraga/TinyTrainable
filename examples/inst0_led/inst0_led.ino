2e65fd3c7b63d84cf112a2f4196624ab5dc5f390
montoyamoraga <montoyamoraga@gmail.com>
2021-03-04 17:39:02 -0500

    add header

--- examples/inst0_led/inst0_led.ino
+++ examples/inst0_led/inst0_led.ino
@@ -1,3 +1,8 @@
+/// @file inst0_led.ino
+/// @brief Example: Instrument0 with output LED
+/// @author montoyamoraga, peter-parque, maxzwang
+/// @date March 2021
+
 void setup() {
   // put your setup code here, to run once:
 }

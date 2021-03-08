f5c9ebf6a4a282182bfb36b5e0432abf3e8257e2
Peter Tone <peter2parque@gmail.com>
2021-03-07 22:43:00 -0500

    complete inst0_midi example

--- examples/inst0/inst0_midi/inst0_midi.ino
+++ examples/inst0/inst0_midi/inst0_midi.ino
@@ -18,7 +18,10 @@ float COLOR_THRESHOLD = 0.5;
 void setup() {
   tiny.setupInstrument(true);
 
-  tiny.setupOutputMIDI(10, 127, 37, 38, 39);
+  tiny.setupOutputMIDI(10, 127);
+  tiny.setMIDINotes(0, 37);
+  tiny.setMIDINotes(1, 38);
+  tiny.setMIDINotes(2, 39);
 
   String objectNames[3] = {"Object 0", "Object 1", "Object 2"};
   tiny.trainKNN(K, EXAMPLES_PER_CLASS, COLOR_THRESHOLD, objectNames);

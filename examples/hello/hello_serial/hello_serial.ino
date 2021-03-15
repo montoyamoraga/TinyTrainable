ef8afcedd85baa112475b4b1a1d1b29210f6a697
Peter Tone <peter2parque@gmail.com>
2021-03-14 21:46:13 -0400

    add hello_serial example

---
+++ examples/hello/hello_serial/hello_serial.ino
@@ -0,0 +1,20 @@
+/// @file hello_serial.ino
+/// @brief Example: Test serial USB output
+
+#include <TinyTrainable.h>
+
+TinyTrainable tiny;
+
+void setup() {
+  tiny.setupOutputSerialUSB();
+}
+
+void loop() {
+  // the object classification number is the only thing sent over serial
+  tiny.playOutput(0);
+  tiny.playOutput(1);
+  tiny.playOutput(2);
+
+  // but you can send other messages directly too
+  Serial.println("Hello, Serial!");
+}

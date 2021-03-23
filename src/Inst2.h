15a39ba54c5f548362a1089fedf5affcbd5c3c38
montoyamoraga <montoyamoraga@gmail.com>
2021-03-23 18:23:56 -0400

    add some comments about libraries

--- src/Inst2.h
+++ src/Inst2.h
@@ -4,8 +4,13 @@
 #ifndef INST2_H
 #define INST2_H
 
-#include "TinyTrainable.h"
+// include Arduino library
 #include <Arduino.h>
+
+// include TinyTrainable library
+#include "TinyTrainable.h"
+
+// include TensorFlow Lite Micro library
 #include <TensorFlowLite.h>
 #include <tensorflow/lite/micro/all_ops_resolver.h>
 #include <tensorflow/lite/micro/micro_error_reporter.h>

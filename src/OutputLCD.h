cf4a45978803a766f1bf482a10b9f3a55317ef27
montoyamoraga <montoyamoraga@gmail.com>
2021-03-26 18:59:02 -0400

    add new placeholders

---
+++ src/OutputLCD.h
@@ -0,0 +1,8 @@
+#ifndef OUTPUT_LCD_H
+#define OUTPUT_LCD_H
+
+class OutputLCD : public TinyTrainable {
+
+};
+
+#endif
---
+++ src/OutputLED.h
@@ -0,0 +1,8 @@
+#ifndef OUTPUT_LED_H
+#define OUTPUT_LED_H
+
+class OutputLED : public TinyTrainable {
+
+};
+
+#endif

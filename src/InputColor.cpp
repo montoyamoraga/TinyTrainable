40ee08ac10f05d5c215b80421ada3cfd39a2cde4
aaron montoya-moraga <montoyamoraga@gmail.com>
2021-03-27 00:52:44 -0400

    move from Inst0 to InputColor

--- src/InputColor.cpp
+++ src/InputColor.cpp
@@ -7,3 +7,47 @@ InputColor::InputColor() : _myKNN(3) {
   _labels[1] = "";
   _labels[2] = "";
 }
+
+// true: instrument outputs debugging messages over USB serial
+// false: standalone instrument, no debugging message
+void InputColor::setupInstrument(bool serialDebugging) {
+  setupLEDs();
+  setupSensorAPDS9960();
+
+  _serialDebugging = serialDebugging;
+
+  if (_serialDebugging) {
+    Serial.begin(9600);
+    while (!Serial)
+      ;
+  }
+}
+
+// reads the color from the color sensor
+// stores the rgb values in 'colorReading[]'
+void InputColor::readColor(float colorReading[]) {
+
+  // declare and initialize local variables for color
+  int red, green, blue, colorTotal = 0;
+
+  // wait for the object to move close enough
+  while (!APDS.proximityAvailable() || APDS.readProximity() > 0) {
+  }
+
+  // wait until the color is bright enough
+  while (colorTotal < _colorThreshold) {
+
+    // sample if the color is available and the object is close
+    if (APDS.colorAvailable()) {
+
+      // read color and proximity
+      APDS.readColor(red, green, blue);
+      colorTotal = (red + green + blue);
+
+      // update readings
+      _colorReading[0] = red;
+      _colorReading[1] = green;
+      _colorReading[2] = blue;
+    }
+  }
+}
--- src/Inst0.cpp
+++ src/Inst0.cpp
@@ -1,19 +1,3 @@
-
-// true: instrument outputs debugging messages over USB serial
-// false: standalone instrument, no debugging message
-void Inst0::setupInstrument(bool serialDebugging) {
-  setupLEDs();
-  setupSensorAPDS9960();
-
-  _serialDebugging = serialDebugging;
-
-  if (_serialDebugging) {
-    Serial.begin(9600);
-    while (!Serial)
-      ;
-  }
-}
-
 // trains the KNN algorithm with examples provided by the user
 // algorithm will use the 'k' nearest neighbors for classification
 // 'examplesPerClass' examples that pass 'colorThreshold' are collected per
@@ -110,35 +94,6 @@ void Inst0::identify() {
 
   // update previous classification
   _previousClassification = classification;
-}
-
-// reads the color from the color sensor
-// stores the rgb values in 'colorReading[]'
-void Inst0::readColor(float colorReading[]) {
-
-  // declare and initialize local variables for color
-  int red, green, blue, colorTotal = 0;
-
-  // wait for the object to move close enough
-  while (!APDS.proximityAvailable() || APDS.readProximity() > 0) {
-  }
-
-  // wait until the color is bright enough
-  while (colorTotal < _colorThreshold) {
-
-    // sample if the color is available and the object is close
-    if (APDS.colorAvailable()) {
-
-      // read color and proximity
-      APDS.readColor(red, green, blue);
-      colorTotal = (red + green + blue);
-
-      // update readings
-      _colorReading[0] = red;
-      _colorReading[1] = green;
-      _colorReading[2] = blue;
-    }
-  }
 }
 
 // TODO: docstring, call this at the beginning of identify() once

2d1d037ce632acdeb36692ffe2046cf4cc25ec0f
montoyamoraga <montoyamoraga@gmail.com>
2021-03-29 21:09:46 -0400

    add conditional endif for conditional compilation of tensorflow

--- src/InputSpeech.h
+++ src/InputSpeech.h
@@ -11,12 +11,14 @@
 // #include "TinyTrainable.h"
 
 // include libraries for speech recognition
+#ifdef INPUT_SPEECH
 #include <TensorFlowLite.h>
 #include <tensorflow/lite/micro/all_ops_resolver.h>
 #include <tensorflow/lite/micro/micro_error_reporter.h>
 #include <tensorflow/lite/micro/micro_interpreter.h>
 #include <tensorflow/lite/schema/schema_generated.h>
 #include <tensorflow/lite/version.h>
+#endif
 
 class InputSpeech : public Input {
 public:

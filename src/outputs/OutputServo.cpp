ea907ad0bd3f3100a424c5f4d7a5d130a227df94
montoyamoraga <montoyamoraga@gmail.com>
2021-04-04 14:05:38 -0400

    add some comments

--- src/outputs/OutputServo.cpp
+++ src/outputs/OutputServo.cpp
@@ -1,16 +1,22 @@
 // include header file
 #include "OutputServo.h"
 
+
+// constructor
 OutputServo::OutputServo() {}
 
 void OutputServo::setupOutputServo(int outputPin, int angleMin,
                                      int angleMax) {
   // TODO: add comments about each line
 
+  // define output servo
   _outputPinServo = outputPin;
+
+  // define angleMin and angleMax
   _servoAngleMin = angleMin;
   _servoAngleMax = angleMax;
 
+  // pinMode is an Arduino function
   pinMode(_outputPinServo, OUTPUT);
   _servo.attach(_outputPinServo);
 }

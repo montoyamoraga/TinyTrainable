cf88d91e1bc128d156b1c3b018fcc01d25b032d1
montoyamoraga <montoyamoraga@gmail.com>
2021-04-12 10:29:05 -0400

    add tensorflow includes

--- src/inputs/InputSpeech.h
+++ src/inputs/InputSpeech.h
@@ -25,25 +25,6 @@
 #include "../tensorflow/micro_features_model.h"
 #include "../tensorflow/recognize_commands.h"
 
-// Globals, used for compatibility with Arduino-style sketches.
-namespace {
-tflite::ErrorReporter* error_reporter = nullptr;
-const tflite::Model* model = nullptr;
-tflite::MicroInterpreter* interpreter = nullptr;
-TfLiteTensor* model_input = nullptr;
-FeatureProvider* feature_provider = nullptr;
-RecognizeCommands* recognizer = nullptr;
-int32_t previous_time = 0;
-
-// Create an area of memory to use for input, output, and intermediate arrays.
-// The size of this will depend on the model you're using, and may need to be
-// determined by experimentation.
-constexpr int kTensorArenaSize = 10 * 1024;
-uint8_t tensor_arena[kTensorArenaSize];
-int8_t feature_buffer[kFeatureElementCount];
-int8_t* model_input_buffer = nullptr;
-}  // namespace
-
 class InputSpeech : public Input {
 public:
 };

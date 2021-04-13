0252c6507be986d072dae3cf0689991935b193d5
montoyamoraga <montoyamoraga@gmail.com>
2021-04-13 12:24:31 -0400

    rename folder to micro_speech and add tensorflow/ from Arduino TensorFlow

--- src/inputs/InputGesture.h
+++ src/inputs/InputGesture.h
@@ -19,12 +19,12 @@
 #include <Arduino_LSM9DS1.h>
 
 // include libraries for gesture recognition
-#include <TensorFlowLite.h>
-#include <tensorflow/lite/micro/all_ops_resolver.h>
-#include <tensorflow/lite/micro/micro_error_reporter.h>
-#include <tensorflow/lite/micro/micro_interpreter.h>
-#include <tensorflow/lite/schema/schema_generated.h>
-#include <tensorflow/lite/version.h>
+// #include <TensorFlowLite.h>
+// #include <tensorflow/lite/micro/all_ops_resolver.h>
+// #include <tensorflow/lite/micro/micro_error_reporter.h>
+// #include <tensorflow/lite/micro/micro_interpreter.h>
+// #include <tensorflow/lite/schema/schema_generated.h>
+// #include <tensorflow/lite/version.h>
 
 class InputGesture : public Input {
 public:
--- src/inputs/InputSpeech.h
+++ src/inputs/InputSpeech.h
@@ -11,19 +11,19 @@
 #include "TinyTrainable.h"
 
 // include libraries for speech recognition
-#include <TensorFlowLite.h>
-#include <tensorflow/lite/micro/all_ops_resolver.h>
-#include <tensorflow/lite/micro/micro_error_reporter.h>
-#include <tensorflow/lite/micro/micro_interpreter.h>
-#include <tensorflow/lite/schema/schema_generated.h>
-#include <tensorflow/lite/version.h>
+// #include <TensorFlowLite.h>
+// #include <tensorflow/lite/micro/all_ops_resolver.h>
+// #include <tensorflow/lite/micro/micro_error_reporter.h>
+// #include <tensorflow/lite/micro/micro_interpreter.h>
+// #include <tensorflow/lite/schema/schema_generated.h>
+// #include <tensorflow/lite/version.h>
 
-#include "../tensorflow/main_functions.h"
-#include "../tensorflow/command_responder.h"
-#include "../tensorflow/feature_provider.h"
-#include "../tensorflow/micro_features_micro_model_settings.h"
-#include "../tensorflow/micro_features_model.h"
-#include "../tensorflow/recognize_commands.h"
+// #include "../tensorflow/main_functions.h"
+// #include "../tensorflow/command_responder.h"
+// #include "../tensorflow/feature_provider.h"
+// #include "../tensorflow/micro_features_micro_model_settings.h"
+// #include "../tensorflow/micro_features_model.h"
+// #include "../tensorflow/recognize_commands.h"
 
 class InputSpeech : public Input {
 public:

accd379a84baec086b939037ca2e312824c677c7
montoyamoraga <montoyamoraga@users.noreply.github.com>
2021-04-15 22:36:46 +0000

    deploy: fe8f4f504743c58ea1b2908118320c72e742ea5b

--- hello__midi_8ino.js
+++ hello__midi_8ino.js
@@ -1,11 +1,12 @@
 var hello__midi_8ino =
 [
-    [ "TINY_OUTPUT", "hello__midi_8ino.html#a918597ff926f0b9458d71e0811f33989", null ],
+    [ "INPUT_NONE", "hello__midi_8ino.html#a2415dbc707abe01ed8d1b9925681d65c", null ],
+    [ "OUTPUT_MIDI", "hello__midi_8ino.html#a2a1fb658efcdd8388d916148449dab1a", null ],
     [ "loop", "hello__midi_8ino.html#afe461d27b9c48d5921c00d521181f12f", null ],
+    [ "myTiny", "hello__midi_8ino.html#a99537d10ffd272e97cce9933dd325a19", null ],
     [ "setup", "hello__midi_8ino.html#a4fc01d736fe50cf5b977f755b675f11d", null ],
     [ "midiChannel", "hello__midi_8ino.html#aa7d9728ac8a774a923e79270f3663b14", null ],
     [ "MIDINotes", "hello__midi_8ino.html#a1dc4ed62a75287d73d62923f50cbec8c", null ],
     [ "midiVelocity", "hello__midi_8ino.html#a45329bedfdd76f14b24f360555663f54", null ],
-    [ "pauseTime", "hello__midi_8ino.html#ae08d2c382b1bc1b2351974750116c861", null ],
-    [ "tiny", "hello__midi_8ino.html#a9259a888c3fc1d4b52242d5b87014fcc", null ]
+    [ "pauseTime", "hello__midi_8ino.html#ae08d2c382b1bc1b2351974750116c861", null ]
 ];
--- search/defines_3.js
+++ search/defines_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['tiny_5foutput_423',['TINY_OUTPUT',['../hello__midi_8ino.html#a918597ff926f0b9458d71e0811f33989',1,'TINY_OUTPUT():&#160;hello_midi.ino'],['../hello__printer_8ino.html#a918597ff926f0b9458d71e0811f33989',1,'TINY_OUTPUT():&#160;hello_printer.ino'],['../hello__serial_8ino.html#a918597ff926f0b9458d71e0811f33989',1,'TINY_OUTPUT():&#160;hello_serial.ino'],['../hello__tiny__world_8ino.html#a918597ff926f0b9458d71e0811f33989',1,'TINY_OUTPUT():&#160;hello_tiny_world.ino']]]
+  ['tiny_5foutput_423',['TINY_OUTPUT',['../hello__printer_8ino.html#a918597ff926f0b9458d71e0811f33989',1,'TINY_OUTPUT():&#160;hello_printer.ino'],['../hello__serial_8ino.html#a918597ff926f0b9458d71e0811f33989',1,'TINY_OUTPUT():&#160;hello_serial.ino'],['../hello__tiny__world_8ino.html#a918597ff926f0b9458d71e0811f33989',1,'TINY_OUTPUT():&#160;hello_tiny_world.ino']]]
 ];

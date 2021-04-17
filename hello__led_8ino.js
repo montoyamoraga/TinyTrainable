801ba2ba94b31cc25c8227a2a48330d45fda07bd
montoyamoraga <montoyamoraga@users.noreply.github.com>
2021-04-17 15:50:19 +0000

    deploy: 274220fb1c0e4c6cd30c258e674512f3ace6023c

--- hello__led_8ino.js
+++ hello__led_8ino.js
@@ -5,10 +5,9 @@ var hello__led_8ino =
     [ "loop", "hello__led_8ino.html#afe461d27b9c48d5921c00d521181f12f", null ],
     [ "myTiny", "hello__led_8ino.html#a2d1522aedb1701a2d59785e46b770c9c", null ],
     [ "setup", "hello__led_8ino.html#a4fc01d736fe50cf5b977f755b675f11d", null ],
-    [ "myInput", "hello__led_8ino.html#ad04c39d6e2361dadff4d8b1a5abb46f9", null ],
-    [ "myOutput", "hello__led_8ino.html#ab576bf850058fedce7ff70e05f0a2591", null ],
     [ "outputPinLED0", "hello__led_8ino.html#a6a50062bdfa3138b617e2a602c1f05fc", null ],
     [ "outputPinLED1", "hello__led_8ino.html#a659b3275682883828969ebfa5ac2312f", null ],
     [ "outputPinLED2", "hello__led_8ino.html#a2658fbbde3e4b728539d3ec60797c982", null ],
-    [ "pauseTime", "hello__led_8ino.html#a82b4b9ae33c6bfc64cf9a0cae15e7d5e", null ]
+    [ "pauseTime", "hello__led_8ino.html#a82b4b9ae33c6bfc64cf9a0cae15e7d5e", null ],
+    [ "tinyDebugging", "hello__led_8ino.html#a24c8f6b6eb712c7c2c219723c7b05ef0", null ]
 ];

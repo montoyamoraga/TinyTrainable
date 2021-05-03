e2a972c314dd668f51fa45b87fcc22e6c930bc9b
montoyamoraga <montoyamoraga@gmail.com>
2021-05-03 15:16:23 -0400

    add placeholder

---
+++ examples/color/color_printer/color_printer.ino
@@ -0,0 +1,58 @@
+/// @file color_printer.ino
+/// @brief input color with output thermal printer
+
+// define input and output
+#define INPUT_COLOR
+#define OUTPUT_PRINTER
+
+// include TinyTrainable library
+#include <TinyTrainable.h>
+
+// declare instance of a TinyTrainable instrument
+TinyTrainable myTiny(new InputColor(), new OutputPrinter());
+
+// constants for the KNN algorithm
+const int K = 5;
+const int EXAMPLES_PER_CLASS = 10;
+const float COLOR_THRESHOLD = 0.5;
+
+String objectNames[3] = {"Object 0", "Object 1", "Object 2"};
+
+// constants for the hardware
+const int placeHolder0 = 10;
+const int placeHolder1 = 10;
+const int placeHolder2 = 10;
+
+// TODO: move this to library
+// instance of Adafruit Thermal Printer, connected to Serial1
+// Adafruit_Thermal printer(&Serial1);
+
+void setup() {
+
+  // TODO: baudrate is 19200, or 9600 for some printers, confirm
+  //  Serial1.begin(19200);
+
+  // start printer
+  //  printer.begin();
+
+  // text justification
+  // TODO: what is F()?
+  //  printer.justify('R');
+  //  printer.println(F("Right justified"));
+  //  printer.justify('C');
+  //  printer.println(F("Center justified"));
+  //  printer.justify('L');
+  //  printer.println(F("Left justified"));
+
+  // make printer sleep for 5 seconds
+  //  printer.sleep();
+  // TODO: what is L?
+  //  delay(5000L);
+  // wake up the printer
+  //  printer.wake();
+
+  // restore printer to defaults
+  //  printer.setDefault();
+}
+
+void loop() {}

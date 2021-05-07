e47cbeec9d7777a2f082f756ec2e9e14296980a6
montoyamoraga <montoyamoraga@users.noreply.github.com>
2021-05-07 17:39:07 +0000

    deploy: d2a409851aa6c8c6505772b229580fcf2eae03ba

--- class_input.js
+++ class_input.js
@@ -3,9 +3,9 @@ var class_input =
     [ "~Input", "class_input.html#a230646fa6e6433b4d9ac3535b7d52087", null ],
     [ "gesturePrintHeader", "class_input.html#a205ab859966e4373e9a8242345d68a63", null ],
     [ "gestureReadData", "class_input.html#a0d990222b6073894c179e45a3f276a3e", null ],
+    [ "gestureSetupTF", "class_input.html#a1fb01af872f15e6d0b8d21838d39c760", null ],
     [ "identify", "class_input.html#a90132889a3e2ef01e864fa496c63c2e7", null ],
     [ "setupInstrument", "class_input.html#a6f220870bd53dfd6c68ef398c8644a3e", null ],
-    [ "setupTF", "class_input.html#ae11b1c2e750a3075b9b95aecf963d08d", null ],
     [ "trainKNN", "class_input.html#a3ddaa7f2974b49cb5ba74d83dc069bcd", null ],
     [ "tiny", "class_input.html#ab56185d2fbad06c2acb0b84dc11e5199", null ]
 ];

88adaf1c08b81ee79ce9f556547d75359f1f7655
montoyamoraga <montoyamoraga@gmail.com>
2021-05-08 14:41:42 -0400

    fix bug by commenting placeholder line

--- src/outputs/OutputBuzzer.cpp
+++ src/outputs/OutputBuzzer.cpp
@@ -7,7 +7,7 @@
 OutputBuzzer::OutputBuzzer() {}
 
 void OutputBuzzer::setupOutputTest() {
-  tone(_outputPinTest, 260, timeDelay);
+//   tone(_outputPinTest, 260, timeDelay);
 //     delay(timeDelay);
 //     tone(_outputPinTest, 330, timeDelay);
 //     delay(timeDelay);

0ba1a0a1bf48c83afa513d7d2a38ec17b146fffd
montoyamoraga <montoyamoraga@gmail.com>
2021-05-08 21:23:52 -0400

    add libraries for screen

--- src/outputs/OutputScreen.h
+++ src/outputs/OutputScreen.h
@@ -13,6 +13,18 @@
 /// @include library TinyTrainable
 #include "TinyTrainable.h"
 
+/// @include library SPI for screen
+#include <SPI.h>
+
+/// @include library Wire for screen
+#include <Wire.h>
+
+/// @include library Adafruit_GFX for screen
+#include <Adafruit_GFX.h>
+
+/// @include library Adafruit_SSD1306 for screen
+#include <Adafruit_SSD1306.h>
+
 /// @class OutputScreen
 /// @brief OutputScreen inherits from Output
 class OutputScreen : public Output {

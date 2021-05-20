63ae3d6b912e630ebc26132dc8702b66121eb29b
montoyamoraga <montoyamoraga@gmail.com>
2021-05-20 17:51:18 -0400

    add placeholder examples

---
+++ examples/speech/speech_midi/speech_midi.ino
@@ -0,0 +1,23 @@
+// include library TinyTrainable
+#include <TinyTrainable.h>
+
+// declare instance of a TinyTrainable instrument
+TinyTrainable myTiny(INPUT_SPEECH, OUTPUT_MIDI);
+
+// constants for the hardware
+const int outputPin = 8;
+
+// constant for debugging
+const bool debugOn = true;
+
+String myItems[3] = {"item0", "item1", "item2"};
+
+void setup() {
+  // set debugging over serial port
+  myTiny.setupInstrument(debugOn);
+
+  // set its output
+  //   myTiny.setupOutputBuzzer(outputPin);
+}
+
+void loop() {}

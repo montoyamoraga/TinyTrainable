65ab49ee8d0873139db73712da982e81fce2c9a4
montoyamoraga <montoyamoraga@gmail.com>
2021-05-20 17:46:02 -0400

    add gesture_printer

---
+++ examples/gesture/gesture_printer/gesture_printer.ino
@@ -0,0 +1,43 @@
+// this sketch is adapted from the GestureToEmoji example
+// from the repository ArduinoTensorFlowLiteTutorials by Arduino
+// https://github.com/arduino/ArduinoTensorFlowLiteTutorials
+
+// TODO: rewrite this placeholder code
+// at this stage, this code is a placeholder,
+// copied from the following file
+// https://github.com/arduino/ArduinoTensorFlowLiteTutorials/blob/master/GestureToEmoji/ArduinoSketches/IMU_Classifier/IMU_Classifier.ino
+// with only little modifications
+// and still without comments about what most lines mean
+// and without optimization
+
+// include gesture model
+#include "myGestureModel.h"
+
+// include library TinyTrainable
+#include <TinyTrainable.h>
+
+// declare instance of a TinyTrainable instrument
+TinyTrainable myTiny(INPUT_GESTURE, OUTPUT_PRINTER);
+
+// constant for debugging
+const bool debugOn = true;
+
+// array to map gesture index to a name
+String GESTURES[] = {"gesture0", "gesture1", "gesture2"};
+
+void setup() {
+  // setup instrument
+  myTiny.setupInstrument(debugOn);
+
+  // use this line to include the default model
+  myTiny.setupGestureModel(GESTURES);
+  // or use this line to include your own model
+  //   myTiny.setupGestureModel(GESTURES, myGestureModel);
+
+  //  myTiny.setupOutputSerial();
+}
+
+void loop() {
+  // run the classification
+  myTiny.identify();
+}

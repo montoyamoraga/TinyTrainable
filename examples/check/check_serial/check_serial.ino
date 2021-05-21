07950dc9b70c8cfde3faf57d5917b5283a1e9c1a
montoyamoraga <montoyamoraga@gmail.com>
2021-05-21 10:35:14 -0400

    rename hello to check

---
+++ examples/check/check_serial/check_serial.ino
@@ -0,0 +1,50 @@
+// include library TinyTrainable
+#include <TinyTrainable.h>
+
+// declare instance of a TinyTrainable instrument
+TinyTrainable myTiny(INPUT_NONE, OUTPUT_SERIAL);
+
+int pauseTime = 500;
+
+void setup() {
+  // setup output
+  myTiny.setupOutputSerial();
+}
+
+void loop() {
+  // lets send the classification number over serial
+  myTiny.playOutput(0);
+  delay(pauseTime);
+  myTiny.playOutput(1);
+  delay(pauseTime);
+  myTiny.playOutput(2);
+  delay(pauseTime);
+
+  // you can send other messages too
+  Serial.println("hi! :)");
+  delay(pauseTime);
+
+  // and turn on and off the orange built-in LED
+  myTiny.setStateLEDBuiltIn(true);
+  delay(pauseTime);
+  myTiny.setStateLEDBuiltIn(false);
+  delay(pauseTime);
+
+  // and cycle through the 6 colors of the RGB LED
+  myTiny.setStateLEDRGB(true, red);
+  delay(pauseTime);
+  myTiny.setStateLEDRGB(true, green);
+  delay(pauseTime);
+  myTiny.setStateLEDRGB(true, blue);
+  delay(pauseTime);
+  myTiny.setStateLEDRGB(true, yellow);
+  delay(pauseTime);
+  myTiny.setStateLEDRGB(true, magenta);
+  delay(pauseTime);
+  myTiny.setStateLEDRGB(true, cyan);
+  delay(pauseTime);
+
+  // to turn off the RGB LED, color is irrelevant
+  myTiny.setStateLEDRGB(false, red);
+  delay(pauseTime);
+}

10e1f2c895ba35ec26df524e851e8f9aacd3cbf2
montoyamoraga <montoyamoraga@gmail.com>
2021-06-23 14:00:34 -0400

    add wiring comment to check_servo

--- examples/check/check_servo/check_servo.ino
+++ examples/check/check_servo/check_servo.ino
@@ -4,17 +4,24 @@
 // declare instance of a TinyTrainable instrument
 TinyTrainable myTiny(INPUT_NONE, OUTPUT_SERVO);
 
-// our servo moves between the minimum and maximum angles
+// servo has 3 cables:
+// yellow: signal
+// orange: 3.3 V
+// brown: ground
+
+// the servo moves between the minimum and maximum angles
 // at a desired tempo measured in beats per minute
 const int tempo0 = 20;
 const int tempo1 = 90;
 const int tempo2 = 200;
+
 // constants for the hardware
-const int outputPin = 9;
+// connect servo signal pin to D9 on the Arduino
+const int signalPin = 9;
 
 void setup() {
 
-  myTiny.setupOutputServo(outputPin);
+  myTiny.setupOutputServo(signalPin);
 
   // set the tempo (in bpm) for each object
   myTiny.setServoTempo(0, tempo0);
@@ -23,8 +30,9 @@ void setup() {
 }
 
 void loop() {
+  
   // to test the different tempos, try each output
-  //  myTiny.playOutput(0);
-  myTiny.playOutput(1);
+  myTiny.playOutput(0);
+  // myTiny.playOutput(1);
   // myTiny.playOutput(2);
 }

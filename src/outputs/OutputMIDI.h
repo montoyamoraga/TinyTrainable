1aaa7baf576a0cde4cf7254b4d5a720e64a58016
montoyamoraga <montoyamoraga@gmail.com>
2021-07-01 14:54:29 -0400

    add all notes for korg volca beats

--- src/outputs/OutputMIDI.h
+++ src/outputs/OutputMIDI.h
@@ -43,14 +43,17 @@ public:
   // notes for volca beats
   byte noteBassDrum = 36;
   byte noteSnare = 38;
+  byte noteClap = 39;
   byte noteClosedHiHat = 42;
+  byte noteLowTom = 43;
+  byte noteOpenHiHat = 46;
   byte noteHiTom = 50;
 
   int currentStep = 0;
   int totalSteps = 16;
 
   // 0 - bossanova, 1 - chacha, 2 - samba
-  int currentGenre = 2;
+  int currentGenre = -1;
 
   int tempoDelay = 100;
 };
